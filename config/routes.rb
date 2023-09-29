719dedeb2c04de3891ed31f4ed852e9b3867e8a7
shaswat-satyam <70892928+shaswat-satyam@users.noreply.github.com>
2023-09-29 18:06:57 +0530

    Implemented the attendance table

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events
   resources :users
+  resources :attendance
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
